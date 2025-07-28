c7154737c7ddf4b824b7a76de9ab3b6e8d224ebc
Shah Smit Ajit <111108187+shahsmit2121@users.noreply.github.com>
2025-07-28 20:26:26 +0530

    Time: 488 ms (51.31%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,17 @@
+WITH PreviousWeatherData AS (
+    SELECT 
+        id,
+        recordDate,
+        temperature, 
+        LAG(temperature, 1) OVER (ORDER BY recordDate) AS PreviousTemperature,
+        LAG(recordDate, 1) OVER (ORDER BY recordDate) AS PreviousRecordDate
+    FROM 
+        Weather
+)
+SELECT 
+    id 
+FROM 
+    PreviousWeatherData
+WHERE 
+    temperature > PreviousTemperature
+    AND recordDate = DATEADD(DAY, 1, PreviousRecordDate);
