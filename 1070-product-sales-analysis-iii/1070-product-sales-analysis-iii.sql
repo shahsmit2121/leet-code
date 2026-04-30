a7df6c96afba4267761a6e595446c420dd439c7f
Shah Smit Ajit <111108187+shahsmit2121@users.noreply.github.com>
2026-04-30 15:01:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,23 +1,11 @@
-WITH first_year_table AS (
-SELECT
+SELECT 
     product_id,
-    MIN(year) AS first_year
-FROM 
-    Sales
-GROUP BY
-    product_id
-)
-
-SELECT
-    s.product_id,
-    f.first_year AS first_year,
-    SUM(s.quantity) AS quantity,
-    s.price
-FROM 
-    Sales AS s
-INNER JOIN
-    first_year_table AS f
-    ON s.product_id = f.product_id
-    AND s.year = f.first_year
-GROUP BY
-    s.product_id, f.first_year, s.price
+    year AS first_year,
+    quantity,
+    price
+FROM (
+    SELECT *,
+           MIN(year) OVER (PARTITION BY product_id) AS first_year
+    FROM Sales
+) t
+WHERE year = first_year;
