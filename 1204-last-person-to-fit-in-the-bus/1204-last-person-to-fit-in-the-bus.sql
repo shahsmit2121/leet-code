34f158779892eba3538f0bfa9d79d3b55b1cdf3e
Shah Smit Ajit <111108187+shahsmit2121@users.noreply.github.com>
2026-05-01 21:04:14 +0530

    Time: 1188 ms (47.75%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,18 +1,14 @@
-/* Write your T-SQL query statement below */
-WITH cumm_weight AS (
+SELECT TOP 1
+    person_name
+FROM
+    (
     SELECT 
-        person_name
+        *,
         SUM(weight) OVER(ORDER BY turn) as cumulative_weight
     FROM 
         Queue
-)
-
-SELECT
-    person_name
-FROM
-    cumm_weight
+    ) AS t
 WHERE
-    cumulative_weight <= 1000
-    AND turn = (SELECT MAX(turn)
-                FROM cumm_weight
-                WHERE cumulative_weight <= 1000)
+    t.cumulative_weight <= 1000
+ORDER BY
+    t.cumulative_weight DESC
