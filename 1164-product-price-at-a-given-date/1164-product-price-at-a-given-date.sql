39bb9d3613938f3de602e80bfcda140cf296dee3
Shah Smit Ajit <111108187+shahsmit2121@users.noreply.github.com>
2026-05-01 10:46:37 +0530

    Time: 1042 ms (21.53%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,22 @@
+/* Write your T-SQL query statement below */
+WITH latest_price AS (
+    SELECT
+        product_id,
+        new_price,
+        RANK() OVER(PARTITION BY product_id
+                    ORDER BY change_date DESC) AS rn
+    FROM 
+        Products 
+    WHERE 
+        change_date <= '2019-08-16'
+)
+
+SELECT
+    p.product_id,
+    COALESCE(lp.new_price, 10) AS price
+FROM
+    (SELECT DISTINCT product_id FROM Products) AS p
+LEFT JOIN
+    latest_price AS lp
+    ON p.product_id = lp.product_id 
+    AND lp.rn = 1
